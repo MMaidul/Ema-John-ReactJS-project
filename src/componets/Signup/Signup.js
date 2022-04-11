5ab815e9a50948cb1003381b9fef10341fcdf157
MMaidul <maidulmuqtadir360@gmail.com>
2022-04-11 20:12:27 +0600

    sign up firebase auth done

--- src/componets/Signup/Signup.js
+++ src/componets/Signup/Signup.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../firebase/firebase.init'
 import './Signup.css';
+
 const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
+    const navigate = useNavigate()
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
 
     const handleEmailBlur = e => {
         const emailInput = e.target.value;
@@ -19,40 +24,48 @@ const Signup = () => {
         const confirmPasswordInput = e.target.value;
         setConfirmPassword(confirmPasswordInput)
     }
+    if (user) {
+        navigate('/shop')
+    }
     const handleCreateUser = e => {
         e.preventDefault();
         if (password !== confirmPassword) {
             setError('Your two passwords did not match')
             return;
         }
+        if (password.length < 6) {
+            setError('Password must be 6 characters or longer')
+            return;
+        }
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div>
             <div className='form-container'>
-            <div>
-                <h2 className='form-title'>Sign Up</h2>
-                <form onSubmit={handleCreateUser}>
-                <div className="input-group">
-                   <label htmlFor="email">Email</label>
-                   <input onBlur={handleEmailBlur} type="email" name="email" id=""  required/>
-                </div>
-                <div className="input-group">
-                    <label htmlFor="password">Password</label>
-                    <input onBlur={handlePasswordBlur} type="password" name="password" id="" required/>
-                </div>
-                <div className="input-group">
-                    <label htmlFor="confirm-password">confirm-Password</label>
-                    <input onBlur={handleConfirmPassword} type="password" name="confirm-password" id="" required/>
+                <div>
+                    <h2 className='form-title'>Sign Up</h2>
+                    <form onSubmit={handleCreateUser}>
+                        <div className="input-group">
+                            <label htmlFor="email">Email</label>
+                            <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
                         </div>
-                        <p style={{color:'red'}}>{error}</p>
-                <input className='form-submit' type="submit" value="Sign up" />
-                </form>
-                <p>
-                    Already Have An Account? <Link className='form-link' to={"/login"}>Lon in</Link>
-                </p>
-                <p className='optional-link'>Or</p>
+                        <div className="input-group">
+                            <label htmlFor="password">Password</label>
+                            <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="confirm-password">confirm-Password</label>
+                            <input onBlur={handleConfirmPassword} type="password" name="confirm-password" id="" required />
+                        </div>
+                        <p style={{ color: 'red' }}>{error}</p>
+                        <input className='form-submit' type="submit" value="Sign up" />
+                    </form>
+                    <p>
+                        Already Have An Account? <Link className='form-link' to={"/login"}>Lon in</Link>
+                    </p>
+                    <p className='optional-link'>Or</p>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
--- src/componets/_auth_steps.js
+++ src/componets/_auth_steps.js
@@ -1,10 +1,12 @@
 /* 
-1. create a new project in console.google.com
+1. create a new project in console.firebase.google.com
 2.npm install firebase
 3.create firebase.init.js and import getAuth to export auth
 4.Firebase settings > authentication > enable Email and password auth
 5.crate login,signup component, setup route
 6.attach from field handle and from submit handle
+7.npm install --save react-firebase-hooks
+8.useCreateUserWithEmailAndPassword from react-firebase-hooks
 
  */
 
