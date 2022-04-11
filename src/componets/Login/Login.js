cd7ef3ce6ee7825ad0cc71a2d87d54dc35184e4e
MMaidul <maidulmuqtadir360@gmail.com>
2022-04-11 21:22:16 +0600

    impliment login to shop component

--- src/componets/Login/Login.js
+++ src/componets/Login/Login.js
@@ -1,22 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../firebase/firebase.init';
 import './Login.css';
 
 const Login = () => {
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleEmailBlur = e => {
+        const emailInput = e.target.value;
+        setEmail(emailInput)
+    }
+    const handlePasswordBlur = e => {
+        const passwordInput = e.target.value;
+        setPassword(passwordInput)
+    }
+    const handleUserSignIn = e => {
+        e.preventDefault();
+        signInWithEmailAndPassword(email, password)
+    }
+    if (user) {
+        navigate('/shop')
+    }
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>Login</h2>
-                <form action="">
-                <div className="input-group">
-                   <label htmlFor="email">Email</label>
-                   <input type="email" name="email" id="" required />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="" required />
-                </div>
-                <input className='form-submit' type="submit" value="Log in" />
+                <form onSubmit={handleUserSignIn}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                    </div>
+                    <p style={{ color: 'red' }}>{error?.message}</p>
+                    {
+                        loading && <p> loading...</p>
+                    }
+                    <input className='form-submit' type="submit" value="Log in" />
                 </form>
                 <p>
                     New to Ema-John? <Link className='form-link' to={"/signup"}>Create A New Account</Link>
